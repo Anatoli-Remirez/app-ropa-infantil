05bb43c2b5aa6ef591687478012c5bf5a9b091d0
Anatoli Remirez <anatoli.remirez@gmail.com>
2021-08-17 00:01:21 +0200

    Desafio 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 
@@ -13,8 +14,12 @@ function App() {
     <div className="App">
         
         <NavBar title = "Tienda de ropa infantil" subtitle="La mejor" />
-        <ItemListContainer saludo="Hola bienvenido a tu tienda de ropa infantil "/>
-        
+        <div className="botonAgregar">
+        <ItemCount stock="5" initial="1" />
+        </div>
+        <div className="container">
+        <ItemListContainer saludo="Hola bienvenido a tu tienda de ropa infantil "/> 
+        </div>
     </div>
     
   );
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './ItemCount.css'
+
+const ItemCount = ({initial, stock,}) => {
+
+    const [counter, setCounter] = React.useState(1);
+    const [onAdd, setOnAdd] = React.useState();
+
+    let agregar = () =>{
+
+        if(counter <= stock){
+
+            let cantidadAgregar = counter;
+            
+            let resultado = stock - cantidadAgregar;
+            
+            setOnAdd(resultado);
+        }
+        
+    }
+    
+    let aumentarValor = () =>{
+
+        setCounter(counter + 1);
+    }
+
+    
+   
+    let reducirValor = () =>{
+
+        setCounter(counter - 1);
+    }
+
+    if (counter >= stock){
+        aumentarValor = () =>{
+            setCounter(counter)
+        }
+    }
+    
+
+    if (counter <= initial){
+        reducirValor = () =>{
+            setCounter(counter)
+        }
+    }
+
+    return (
+        <>
+        <p className="stockDisponible">Stock disponible: {stock}</p>
+        <p className="agregarAlCarrito">Agregar al carrito: {counter}</p>
+        <p className="unidades-sobrantes">Quedan {onAdd} unidades</p>
+        <button className="botonPlus" onClick={aumentarValor}>+</button>
+        <button className="botonRest" onClick={reducirValor}>-</button>
+        <button className="botonAgregar2" onClick={agregar}>Agregar</button>  
+        </>
+    )
+}
+
+export default ItemCount
